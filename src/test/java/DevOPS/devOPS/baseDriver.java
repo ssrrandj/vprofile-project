1744621c39dbb03a10c87c89cc55495dc9f5f776
ssrrandj <36181778+ssrrandj@users.noreply.github.com>
2024-12-09 00:02:53 +0530

    Update baseDriver.java

--- src/test/java/DevOPS/devOPS/baseDriver.java
+++ src/test/java/DevOPS/devOPS/baseDriver.java
@@ -14,7 +14,7 @@ public class baseDriver {
 	
 	public static WebDriver Chromedriver() {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
-				  + "\\ChromeDriver\\chrome.exe");
+				  + "ChromeDriver/chrome.exe");
 		WebDriver driver = new ChromeDriver();
 		return driver;
 
